6b100c5d8f1996927f86f4549904fb226f13617a
Sahana M <sahanam14537@gamil.com>
2026-09-15 23:21:38 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function submitting()
+{
+    var btn1=document.getElementById("btn1");
+
+    if(btn1.checked==true)
+        alert("Successfully Submitted");
+    else if(name1.checked==false) 
+        alert("Please enter your name");
+    // else if(email1.checked==false)
+    //     alert("Please enter your email");
+    // else if(msg1.checked==false)
+    //     alert("Please enter message")
+    // else if(name1,email.checked==false)
+    //     alert()
+    else
+        alert("Please enter your details")
+}
